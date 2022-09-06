97eabb4647c9bd6d427a7d974727c537d364769a
mrcaseb <roemerle@gmail.com>
2022-09-06 12:53:50 +0200

    update multiple laterals stuff

--- R/update_lat_rec.R
+++ R/update_lat_rec.R
@@ -1,33 +1,48 @@
 save_lateral_yards <- function(s) {
   future::plan("multisession")
+  
   games <- nflreadr::load_schedules(s) |>
     dplyr::filter(!is.na(result))
   
+  if(length(games$game_id) == 0){
+    cli::cli_alert_info("No games yet, nothing to do")
+    return(invisible(NULL))
+  }
+  
+  cli::cli_alert_info("Load {.val {length(games$game_id)}} game{?s} of {.val {s}}")
+  
   load <- furrr::future_map_dfr(games$game_id, check_lateral_yards)
+  
+  cli::cli_alert_info("Process raw data...")
+  
   all <-
     dplyr::bind_rows(
-      readRDS("data/lateral_yards/multiple_lateral_yards.rds"),
+      readr::read_csv("data/lateral_yards/multiple_lateral_yards.csv", show_col_types = FALSE),
       load |>
         nflfastR::decode_player_ids() |>
         dplyr::group_by(game_id, play_id) |>
         dplyr::filter(dplyr::n() > 1) |>
-        dplyr::ungroup()
+        dplyr::ungroup() |> 
+        suppressMessages()
     ) |> 
-    dplyr::distinct()
+    dplyr::distinct() |> 
+    dplyr::arrange(game_id, play_id)
   
-  saveRDS(all, "data/lateral_yards/multiple_lateral_yards.rds")
+  cli::cli_alert_info("Save multiple lateral plays...")
+  
   readr::write_csv(all, "data/lateral_yards/multiple_lateral_yards.csv")
-  closeAllConnections()
+  future::plan("sequential")
+  cli::cli_alert_info("DONE!")
+  return(invisible(all))
 }
 
 check_lateral_yards <- function(id){
+  cli::cli_progress_step("Loading {id}")
   season <- substr(id, 1, 4)
-  path <- "https://github.com/nflverse/nflfastR-raw/raw/master/raw"
-  # raw_data <- nflreadr::rds_from_url(sprintf("%s/%s/%s.rds", path, season, id))
-  con <- url(sprintf("%s/%s/%s.rds", path, season, id))
-  raw_data <- readRDS(con)
-  close(con)
-  plays <- janitor::clean_names(raw_data$data$viewer$gameDetail$plays) |>
+  base_url <- "https://github.com/nflverse/nflfastR-raw/raw/master/raw"
+  load_url <- file.path(base_url, season, paste0(id, ".rds"))
+  raw_data <- nflreadr::rds_from_url(load_url)
+  plays <- janitor::clean_names(raw_data[[1]][[1]]$gameDetail$plays) |>
     dplyr::select(play_id, play_stats)
   stats <- tidyr::unnest(plays, cols = c("play_stats")) |>
     janitor::clean_names() |>
@@ -50,4 +65,4 @@ check_lateral_yards <- function(id){
   return(stats)
 }
 
-save_lateral_yards(nflreadr:::most_recent_season())
+save_lateral_yards(nflreadr::most_recent_season())
