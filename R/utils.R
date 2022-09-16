6e704a0d6919f21b10c3bfdbe649d8245c21c212
mrcaseb <38586519+mrcaseb@users.noreply.github.com>
2022-09-16 14:45:41 +0200

    scripts are deprecated

--- R/utils.R
+++
@@ -1,6 +0,0 @@
-save_empty_stuff <- function(season, type) {
-  saveRDS(tibble::tibble(), glue::glue("data/ngs/ngs_{season}_{type}.rds"))
-  readr::write_csv(tibble::tibble(), glue::glue("data/ngs/ngs_{season}_{type}.csv.gz"))
-}
-todo <- expand.grid(season = 2016:2020, type = c("passing", "rushing", "receiving"))
-purrr::walk2(todo$season, todo$type, save_empty_stuff)
